7e1b7f76021728596a90a61abb35b87da1f15e12
Patricia Garcia <patijoe33@gmail.com>
2019-04-08 09:40:40 +0200

    End time

--- js/main.js
+++ js/main.js
@@ -4,23 +4,24 @@
 const field = document.querySelector('#field');
 const btn = document.querySelector('.btn');
 const text = document.querySelector('.text');
+const counter = document.querySelector('.counter');
 
 // Función número random
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-
+const numRandom = getRandomInt(100);
+console.log(numRandom);
   
 // Función compara numero random con número introducido.
 function compare () {
-    const numRandom = getRandomInt(100);
-    console.log(numRandom);
+
 
     // Crea el párrafo con la pista  
     if(numRandom === parseInt(field.value)) {
-        text.innerHTML = (`¡WOUUU... ERES un portento!`);
-    } else if (numRandom>parseInt(field) ){
+        text.innerHTML = (`¡WOUUU... Eres un portento!`);
+    } else if (numRandom<parseInt(field.value) ){
         text.innerHTML = `¡CACHIS! El número es menor`;
     } else {
         text.innerHTML = `¡UPS! El número es mayor`;
@@ -29,6 +30,16 @@ function compare () {
 
 // Función contador
 const acc = 0;
+
+function countClick () {
+   if (parseInt(field.value) !== numRandom) {
+    acc = acc + 1;
+   } else {
+       acc = acc;
+   } 
+}
+
+counter.innerHTML = countClick(acc);
 
 
 // Función listener
