c43df2859dbe38fee34bed44f80593b4bc877aa1
Patricia Garcia <patijoe33@gmail.com>
2019-04-08 10:17:47 +0200

    Add counter

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const field = document.querySelector('#field');
 const btn = document.querySelector('.btn');
 const text = document.querySelector('.text');
 const counter = document.querySelector('.counter');
+let acc = 0;
+counter.innerHTML = acc;
 
 // Función número random
 function getRandomInt(max) {
@@ -23,24 +25,19 @@ function compare () {
         text.innerHTML = (`¡WOUUU... Eres un portento!`);
     } else if (numRandom<parseInt(field.value) ){
         text.innerHTML = `¡CACHIS! El número es menor`;
+        // El contador escribe solo si el número es mayor o menor
+        acc += 1;
+        counter.innerHTML = acc;
     } else {
         text.innerHTML = `¡UPS! El número es mayor`;
+        acc += 1;
+        counter.innerHTML = acc;
     }
+
+    // si escribe el contador siempre, aciertes o no
+    // acc = acc+1;
+    // counter.innerHTML = acc;
 }
-
-// Función contador
-const acc = 0;
-
-function countClick () {
-   if (parseInt(field.value) !== numRandom) {
-    acc = acc + 1;
-   } else {
-       acc = acc;
-   } 
-}
-
-counter.innerHTML = countClick(acc);
-
 
 // Función listener
 btn.addEventListener(`click`, compare);
