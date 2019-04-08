c95a940cb8539a6373129c767aef58c94662747d
Patricia Garcia <patijoe33@gmail.com>
2019-04-08 08:50:10 +0200

    add constants adn random function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 "use strict";
 // Constantes
+const field = document.querySelector('.field');
+const btn = document.querySelector('.btn');
+const text = document.querySelector('.text');
+
 // Función número random
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+const numRandom = getRandomInt(100);
+console.log(numRandom);
+  
+// Función compara numero random con número introducido.
+
 // Función crea el párrafo con la pista
 // Función contador
+// Acceder al contenido del input y mostrarlo en consola
 // Función listener
