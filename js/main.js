f5029477a4466276e3c2d7670bc238d2238a1932
Patricia Garcia <patijoe33@gmail.com>
2019-04-08 09:14:42 +0200

    Add fuction compare and clue text

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 "use strict";
+
 // Constantes
-const field = document.querySelector('.field');
+const field = document.querySelector('#field');
 const btn = document.querySelector('.btn');
 const text = document.querySelector('.text');
 
@@ -8,12 +9,27 @@ const text = document.querySelector('.text');
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
-const numRandom = getRandomInt(100);
-console.log(numRandom);
+
+
   
 // Función compara numero random con número introducido.
+function compare () {
+    const numRandom = getRandomInt(100);
+    console.log(numRandom);
 
-// Función crea el párrafo con la pista
+    // Crea el párrafo con la pista  
+    if(numRandom === parseInt(field.value)) {
+        text.innerHTML = (`¡WOUUU... ERES un portento!`);
+    } else if (numRandom>parseInt(field) ){
+        text.innerHTML = `¡CACHIS! El número es menor`;
+    } else {
+        text.innerHTML = `¡UPS! El número es mayor`;
+    }
+}
+
 // Función contador
-// Acceder al contenido del input y mostrarlo en consola
+const acc = 0;
+
+
 // Función listener
+btn.addEventListener(`click`, compare);
