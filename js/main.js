d6d3d26d8cf516e9ce5cd8b2cc215a4c45c84374
Patricia Garcia <patijoe33@gmail.com>
2019-04-14 12:42:09 +0200

    Fix Carlos issues

--- js/main.js
+++ js/main.js
@@ -5,41 +5,52 @@ const field = document.querySelector('#field');
 const btn = document.querySelector('.btn');
 const text = document.querySelector('.text');
 const counter = document.querySelector('.counter');
+const numRandom = getRandomInt(100);
+console.log(numRandom);
+let acc = 0;
 
 // Función número random
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-const numRandom = getRandomInt(100);
-console.log(numRandom);
-  
+// Función innerHTML
+function insert (element, phrase) {
+    element.innerHTML = phrase;
+}
+
 // Función compara numero random con número introducido.
 function compare () {
-
+    const fieldValueInt = parseInt(field.value);
 
     // Crea el párrafo con la pista  
-    if(numRandom === parseInt(field.value)) {
-        text.innerHTML = (`¡WOUUU... Eres un portento!`);
-    } else if (numRandom<parseInt(field.value) ){
-        text.innerHTML = `¡CACHIS! El número es menor`;
-    } else {
-        text.innerHTML = `¡UPS! El número es mayor`;
-    }
-}
+    if (field.value === '') {
+        insert (text, `Prueba suerte, introduce un número`);
+    } else if(numRandom === fieldValueInt) {
+        insert (text, `¡WOUUU... Eres un portento!`);
+    } else if (fieldValueInt<0 ){
+        insert (text, `No valen números negativos :)`);
+        // Hago q el contador cuente solo cuando se falla
+        acc += 1;
+        insert (counter, acc);
+    } else if (fieldValueInt>100){
+        insert (text, `El número random es solo hasta 100 ;)`);
+        acc += 1;
+        insert (counter, acc);
+    } else if (numRandom<fieldValueInt ){
+        insert (text, `¡CACHIS! El número es menor`);
+        acc += 1;
+        insert (counter, acc);
+    } else if (numRandom>fieldValueInt ){
+        insert (text, `¡UPS! El número es mayor`);
+        acc += 1;
+        insert (counter, acc);
 
-// Función contador
-const acc = 0;
+}}
 
-function countClick () {
-   if (parseInt(field.value) !== numRandom) {
-    acc = acc + 1;
-   } else {
-       acc = acc;
-   } 
-}
+function active () {
 
-counter.innerHTML = countClick(acc);
+ }
 
 
 // Función listener
